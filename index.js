5824911d2030a3aed5886e0d473b4b438e0a79c6
Ashley Wong <tofudragon333@gmail.com>
2022-05-17 12:48:38 -0400

    commit tree 63cdfe525cf0745b64943d343ceb78b5b3f1abea

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'sarah lee';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+    return leastFavoriteCustomer;
+}
+
